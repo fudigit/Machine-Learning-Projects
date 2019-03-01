235e207737144c5fa49ae512b24fb647cf01d363
fudigit <30851222+fudigit@users.noreply.github.com>
2019-03-01 00:24:52 -0600

    Update Bias_Variance_Tradeoff.py

--- Bias_Variance_Tradeoff.py
+++ Bias_Variance_Tradeoff.py
@@ -31,6 +31,8 @@ df = pd.read_table('http://people.sc.fsu.edu/~jburkardt/datasets/regression/x01.
 df.head()
 df.describe()
 
+
+'''dicovering relationship between 2 variables'''
 # get rid of the large 'outliers'
 sns.lmplot(x='body', y='brain', data=df, ci=None, fit_reg=False)
 df = df[df.body < 200]
@@ -46,10 +48,13 @@ sns.lmplot(x='body', y='brain', data=df, ci=None)
 sns.plt.xlim(-10, 200)
 sns.plt.ylim(-10, 250)
 
+
+'''linear model on different subsets, high bias, low variance'''
 # set a random seed for reproducibility
+#np.random.seed(123456)
 np.random.seed(12345)
 
-# randomly assign every row to either sample 1 or sample 2
+# randomly assign every row to either sample 1 or sample 2, by adding a 3rd feature 'sample''
 df['sample'] = np.random.randint(1, 3, len(df))
 df.head()
 
@@ -58,21 +63,30 @@ sns.lmplot(x='body', y='brain', data=df, ci=None, col='sample')
 sns.plt.xlim(-10, 200)
 sns.plt.ylim(-10, 250)
 
-
+# put them into the same plot:
 # hue='sample' subsets the data by sample and creates a single plot
 sns.lmplot(x='body', y='brain', data=df, ci=None, hue='sample')
 sns.plt.xlim(-10, 200)
 sns.plt.ylim(-10, 250)
+'''
+high bias: model lack of flexibility to fit data well, strong assumption, simple model
+low variance, doesn't change much (compare to high complexity model) by what are sampled
+'''
 
 
+'''8 order polynomial model, high variance, low bias!'''
 sns.lmplot(x='body', y='brain', data=df, ci=None, col='sample', order=8)
 sns.plt.xlim(-10, 200)
 sns.plt.ylim(-10, 250)
+'''
+low bias: match the data quite well!
+high variance: model vary widely when different sample is given
+'''
 
 
+'''2 order polynomial, less bias than linear, less variance than the 8 order polynomial'''
+# note, 2 order relation can convex up or down depends on the data
 sns.lmplot(x='body', y='brain', data=df, ci=None, col='sample', order=2)
 sns.plt.xlim(-10, 200)
 sns.plt.ylim(-10, 250)
-
-
 

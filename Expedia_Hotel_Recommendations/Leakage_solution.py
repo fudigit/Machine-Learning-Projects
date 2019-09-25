8183b264746227b681517565414918fa72831831
fudigit <30851222+fudigit@users.noreply.github.com>
2019-09-24 22:56:16 -0500

    Update Leakage_solution.py

--- Expedia_Hotel_Recommendations/Leakage_solution.py
+++ Expedia_Hotel_Recommendations/Leakage_solution.py
@@ -23,3 +23,38 @@ for i in range(test.shape[0]):
     clusters = find_exact_match(test.iloc[i], match_col)
     pred_labels.append(clusters)
     
+---------------------------
+# python study
+
+df3 = pd.DataFrame([['a','a','b','b','c','d', 'c'],
+                   [1, 3, 5, 7, 9, 2, 4]], index=["alpha", "val"])
+
+df3.groupby(df.loc['alpha'], axis=1).sum()
+
+
+
+################################################
+ df = pd.DataFrame({'A': ['foo', 'bar', 'foo', 'bar',
+                        'foo', 'bar', 'foo', 'foo'],
+                    'B': ['one', 'one', 'two', 'three',
+                          'two', 'two', 'one', 'three'],
+                   'C': np.random.randn(8),
+def get_letter_type(letter):
+    if letter.lower() in 'aeiou':
+        return 'vowel'
+    else:
+        return 'consonant'
+df.groupby(by = get_letter_type, axis ='columns').count()
+                    
+                    
+                    
+##############################################
+                    for i in range(20):
+    try:
+        index = tuple([test[:10].iloc[i][t] for t in match_col])
+        print(index)
+        group = gb.get_group(index)
+        print(group['hotel_cluster'])
+    except:
+        print('error!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+                    'D': np.random.randn(8)})
